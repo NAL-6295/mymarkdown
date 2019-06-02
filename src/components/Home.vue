19020c4246ccba7aba66265581f738b6fccc8c10
nal-6295 <naruhiron@gmail.com>
2019-06-02 23:54:35 +0900

    add property to Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,14 @@
 　<template>
 　 <div id="home">
 　 <h1>{{ msg }}</h1>
-　 <button @click="googleLogin">Google アカウントでログイン</button>
+　 <button @click="googleLogin" v-show="user == undefined" >Google アカウントでログイン</button>
 　 </div>
 　</template>
 　
 　<script>
 　export default {
-　 name: 'home',
+　 name: 'home', 
+   props: ['user'],
 　 data () {
 　 return {
 　 msg: 'Welcome to MyMarkdown'
